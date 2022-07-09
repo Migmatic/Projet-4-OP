63b6ac8108621373de1f94f0dcb797b944ef5bfc
FlipFlop <95119963+Migmatic@users.noreply.github.com>
2022-07-09 12:52:59 +0200

    Update gmaps.js

--- js/gmaps.js
+++ js/gmaps.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 45.769440, lng: 4.830100 },
     zoom: 8,
   });
 }
